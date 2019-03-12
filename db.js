c15bdeb80ad8335706c1847cce61b795973546e4
Yannick Reekmans <reekmans.yannick@gmail.com>
2019-03-12 19:39:38 +0000

    Add v2.17.1

--- db.js
+++ db.js
@@ -3,6 +3,17 @@ var knexMigrator = new KnexMigrator({
     knexMigratorFilePath: __dirname
 });
 
-knexMigrator.init().then(function(){
-    knexMigrator.migrate();
-});
+// check your database health
+knexMigrator.isDatabaseOK()
+    .then(function () {
+        // database is OK
+    })
+    .catch(function (err) {
+        if (err.code === 'DB_NOT_INITIALISED') {
+            knexMigrator.init();
+        }
+
+        if (err.code === 'DB_NEEDS_MIGRATION') {
+            knexMigrator.migrate();
+        }
+    });
